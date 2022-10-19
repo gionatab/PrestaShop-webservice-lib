0066d049feffaf11f44b9c84400450e259e1a2a8
Gionata Bonazzi <gionata.bonazzi@up3up.it>
2022-10-19 16:43:07 +0200

    Migliorata gestione della risposta
    + Salva alcune informazioni sull'ultima richiesta
    + Migliorata gestione della risposta
    + Aggiunta documentazione
    - Risolto bug nella creazione dell'header della richiesta

--- src/PrestashopClient/Client.php
+++ src/PrestashopClient/Client.php
@@ -5,12 +5,25 @@ use Up3Up\PrestashopClient\Exceptions\PrestashopClientException;
 use Up3Up\PrestashopClient\Exceptions\PrestashopResponseException;
 
 class Client {
+    
+    /** Un'istanza di un client GuzzleHttp */
     protected $client;
+
+    /** La chiave di autenticazione per l'API */
     protected $key;
+
+    /** Viene salvato l'ultimo metodo usato, es. GET */
+    protected $lastRequestMethod;
+
+    /** Viene salvata l'ultima risorsa richiesta, es. categories/20 */
+    protected $lastRequestUri;
+
     public function __construct($base_uri, $key)
     {
         $this->key = $key;
         $this->client = new \GuzzleHttp\Client(['base_uri' => $base_uri]);
+        $this->lastRequestMethod = '';
+        $this->lastRequestUri = '';
         
     }
 
@@ -39,34 +52,59 @@ class Client {
         }
     }
 
-    protected function elaborateResponse($uri, \Psr\Http\Message\ResponseInterface $response) {
-        if($response->getStatusCode() == 200) {
-            $xml = $this->parseXML($response->getBody());
-            return $xml;
+    /**
+     * Prende la risposta ricevuta dalla richiesta, e la divide in 3 casi in base al codice della risposta:
+     * - 404: la risorsa esiste, ma l'elemento non è stato trovato, viene restituito NULL;
+     * - 200: la risorsa esiste, e l'elemento è stato trovato. Se il parse del corpo della risposta è stato fatto correttamente, allora restituiamo un XML (SimpleXML);
+     * - altro: Qualsiasi tipo di codice errore al di fuori di 200 e 404, oppure un errore nel parsing del contenuto della risposta, risulta in una eccezione che fornisce dettagli sull'errore.
+     * 
+     * @param \Psr\Http\Message\ResponseInterface $response La risposta HTTP ricevuta in seguito a una richiesta.
+     * @return SimpleXMLElement|null Se tutto va bene e la risposta ha codice 200, viene restituito un SimpleXMLElement (un oggetto che rappresenta un XML). Se la risposta ha codice 404 viene restiuito NULL poiché la risorsa non esiste.
+     * @throws PrestashopClientException Fornisce informazioni su cosa è andato storto nella richiesta e/o nella lettura della risposta.
+     */
+    protected function elaborateResponse(\Psr\Http\Message\ResponseInterface $response) {
+        $error = false;
+        if($response->getStatusCode() == 404) {
+            return null;
         }
-        else if($response->getStatusCode() == 404) {
-            return false;
-        }
-        else {      
+        else {
             try {
                 $xml = $this->parseXML($response->getBody());
-                if(isset($xml->errors->error->message)) {
+                if(isset($xml->errors->error->message)) { //Non controlliamo il codice di errore, sembra stupido ma non si sa mai restituisce un codice 200 OK con un messaggio di errore nel contenuto...
                     $message = (string) $xml->errors->error->message;
+                    $error = true;
                 }
             } catch (PrestashopResponseException $e){
-                $message = 'Risposta senza contenuto.';
+                $message = 'Errore nell\'analisi del contenuto della risposta.';
+                $error = true;
             }
-            throw new PrestashopClientException($response->getStatusCode(), $response->getReasonPhrase(), 'GET', $uri, $message);
+        }
+        if($response->getStatusCode() == 200 && !$error) {
+            return $xml;
+        }
+        else {      
+            throw new PrestashopClientException($response->getStatusCode(), $response->getReasonPhrase(), $this->lastRequestMethod, $this->lastRequestUri, $message);
         }
     }
 
-    public function get($uri, $params = []) {
+    /**
+     * Esegue una richiesta GET.
+     * l'URI non è altro che l'endpoint della REST API, ed è compreso di due elementi: {risorsa}/{id}. Nel caso id sia mancante, è possibile eseguire un GET con {risorsa} per ottenere la lista di tutti gli elementi. ATTENZIONE: l'URI non deve esere l'URL completo, quindi l'URI base del sito non deve essere indicato. In un Prestashop, un URI base è composto da {link_negozio}/api e non deve essere indicato nell'URI.
+     * I parametri non sono altro che gli elementi della query, ovvero tutta la parte dopo il simbolo ? appeso alla fine dell'URI. I parametri vengono passati come coppia chiave/valore, esempio: 'display' => 'full' si trasforma in "{uri}?display=full.
+     *
+     * @param string $uri l'endpoint che indica quale risorsa/elemento richiedere.
+     * @param array $params eventuali parametri della query
+     * @return SimpleXMLElement|null Un XML se la risorsa è stata trovata, altrimenti NULL.
+     * @throws PrestashopClientException Fornisce informazioni su cosa è andato storto nella richiesta e/o nella lettura della risposta.
+     */
+    public function get(string $uri, $params = []) {
+        $this->lastRequestMethod = 'GET';
+        $this->lastRequestUri = $uri;
         $response = $this->client->get($uri, $this->buildOptions($params));
-        return $this->elaborateResponse($uri, $response);
+        return $this->elaborateResponse($response);
     }
 
     public function post($uri, $body, $params = []) {
-        //$response = $this->client
     }
 
     public function put($uri, $body, $params = []) {
@@ -79,7 +117,7 @@ class Client {
 
     protected function buildOptions($params = []) {
         $options = [
-            'auth' => [$this->key, '']
+            'auth' => [$this->key, ''],
             'http_errors' => false //Disattiva eccezioni della libreria, la gestiamo noi.
         ];
         if(!empty($params)) {
